a9b81cb25614192a1ea8be359bc0487bf1edee1b
Eliott <eliott.kerivin@epfl.ch>
2026-08-23 12:59:26 +0200

    Added required standard headers

--- libalgea/tests/test.h
+++ libalgea/tests/test.h
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #define test(t)                                                                \
   do {                                                                         \
     if (!(t)) {                                                                \
